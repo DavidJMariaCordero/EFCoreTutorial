9d3bdfae1925d5f4743d5f95659824b1131e78f4
David Maria <eldavid_46@hotmail.com>
2021-06-08 00:50:59 -0400

    Carga del proyecto con los ejemplos

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,55 @@
+﻿using EFCoreTutorial.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorial.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<StudentAddress> StudentAddresses { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=SchoolDB;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                    .Property(s => s.StudentId)
+                    .HasColumnName("Id")
+                    .HasDefaultValue(0)
+                    .IsRequired();
+
+            modelBuilder.Entity<Student>()
+                    .HasOne<Grade>(s => s.Grade)
+                    .WithMany(g => g.Students)
+                    .HasForeignKey(s => s.GradeId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Student>()
+                    .HasOne<StudentAddress>(s => s.Address)
+                    .WithOne(ad => ad.Student)
+                    .HasForeignKey<StudentAddress>(ad => ad.StudentAddressId);
+
+            modelBuilder.Entity<StudentCourse>().HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            modelBuilder.Entity<StudentCourse>()
+                    .HasOne<Student>(sc => sc.Student)
+                    .WithMany(s => s.StudentCourses)
+                    .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+                    .HasOne<Course>(sc => sc.Course)
+                    .WithMany(s => s.StudentCourses)
+                    .HasForeignKey(sc => sc.CourseId);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using EFCoreTutorial.DAL;
+using EFCoreTutorial.Entidades;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreTutorial
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var std = new Student()
+                {
+                    FirstName = "Bill",
+                    LastName = "Gates"
+                };
+                context.Students.Add(std);
+
+                context.SaveChanges();
+            }
+
+            using (var context = new SchoolContext())
+            {
+                var std = context.Students.First<Student>();
+                std.FirstName = "Steve";
+                context.SaveChanges();
+            }
+
+            using (var context = new SchoolContext())
+            {
+                var std = context.Students.First<Student>();
+                context.Students.Remove(std);
+
+                context.SaveChanges();
+            }
+
+            
+
+            using (var context = new SchoolContext())
+            {
+                var stud = new Student() { StudentId = 1, FirstName = "Bill" };
+                stud.FirstName = "Steve";
+
+                context.Update<Student>(stud);
+                context.SaveChanges();
+            }
+
+            using (var context = new SchoolContext())
+            {
+                var student = new Student()
+                {
+                    StudentId = 50
+                };
+
+                context.Remove<Student>(student);
+
+                context.SaveChanges();
+            }
+
+            using (var context = new SchoolContext())
+            {
+                var student = context.Students.First();
+                DisplayStates(context.ChangeTracker.Entries());
+            }
+
+            using (var context = new SchoolContext())
+            {
+                var students = context.Students
+                   .FromSqlRaw("Select * from Students where Name = 'Bill'")
+                   .ToList();
+            }
+        }
+
+        private static void DisplayStates(IEnumerable<EntityEntry> entries)
+        {
+            foreach (var entry in entries)
+            {
+                Console.WriteLine($"Entity: {entry.Entity.GetType().Name}, State: { entry.State.ToString()}");
+            }
+        }
+    }
+}
